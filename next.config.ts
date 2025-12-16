a9fbe6cf49cde684ba9aa8862d1d40ea72eb78fd
vivekjoshi873 <vivekjoshi873@users.noreply.github.com>
2025-12-16 11:24:48 +0530

    edited in next.config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import path from "node:path";
 
 const nextConfig: NextConfig = {
   images: {
@@ -15,7 +14,6 @@ const nextConfig: NextConfig = {
     ],
     formats: ['image/avif', 'image/webp'],
   },
-  outputFileTracingRoot: path.resolve(__dirname, '../../'),
   typescript: {
     ignoreBuildErrors: true,
   },
